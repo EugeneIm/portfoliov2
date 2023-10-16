74a5519ca50c7cd927e4623a7236a671873052e2
Eugene <immer277@gmail.com>
2023-10-16 10:40:23 -0700

    start of the portfolio rebuild off previous iteration

---
+++ portfolio/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <p>Portfolio V2</p>
+  );
+}
+
+export default App;
---
+++ portfolio/src/content.js
@@ -0,0 +1,128 @@
+const logotext = "🖥";
+const meta = {
+  title: "Quick Portfolio",
+  description: "I'm Eugene Im, a graduate of the SSD program at BCIT.",
+};
+
+const introdata = {
+  title: "Portfolio made in under a day",
+  animated: {
+    first: "I like programming",
+    second: "I like to design and draw",
+    third: "I develop web apps, different types of software, etc.",
+  },
+  description:
+    "This is a general portfolio for a software developer (made quickly).",
+  your_img_url:
+    "https://i.pinimg.com/564x/69/24/ec/6924ec3771635f917e9cea844abfbce1.jpg",
+};
+
+const dataabout = {
+  title: "A little bit about myself.",
+  aboutme:
+    "Originally I wanted to go into the human sciences and not computers/technology. However, as I started to get more and more into computers, video games, etc. I started to get curious about computers. As I became more and more involved in computers whether through games or just understanding them, it became more and more intriguing.",
+};
+
+const skills = [
+  {
+    name: "SQL, mySQL",
+  },
+  {
+    name: "Javascript",
+  },
+  {
+    name: "React",
+  },
+  {
+    name: "React-Native",
+  },
+  {
+    name: "HTML, CSS/SCSS",
+  },
+  {
+    name: "Git",
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Previously was in the D3 program which revolves around UI/UX so I have some experience in that field as D3 was an introduction to UI/UX design.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "We have created mobile apps with different languages so far. As of now we have used Kotlin, React-Native, Flutter, and SwiftUI.",
+  },
+  {
+    title: "Web Apps",
+    description:
+      "Every one of our classes includes creating a web app. Ranging from simple express apps to get information solely through backend with React to Websites with customer registration to a database and requiring authentication.",
+  },
+  {
+    title: "Adobe Photoshop",
+    description:
+      "Previous in D3, along with UI/UX designing, we also learned things such as Adobe Potoshop and a few others. However I only feel comfortable with Photoshop if I were to choose one.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://i.pinimg.com/564x/6d/df/9d/6ddf9d9982a9dcf2d7f1ed8d07ed1049.jpg",
+    desctiption: "Being crazy isn't always bad.",
+    link: "#",
+  },
+  {
+    img: "https://i.pinimg.com/564x/6e/c8/12/6ec8120bb1c5e0de0b339b2aa1346e12.jpg",
+    desctiption: "Life is a gamble.",
+    link: "#",
+  },
+  {
+    img: "https://i.pinimg.com/564x/38/e3/22/38e322b27a65cf8499758554c5365a76.jpg",
+    desctiption: "Music can bring people together.",
+    link: "#",
+  },
+  {
+    img: "https://i.pinimg.com/564x/5e/bb/d0/5ebbd08463dc21a42e28903c1c5a2d59.jpg",
+    desctiption: "Just a Photoshop project I've done in the past.",
+    link: "https://i.pinimg.com/564x/5e/bb/d0/5ebbd08463dc21a42e28903c1c5a2d59.jpg",
+  },
+  {
+    img: "https://i.pinimg.com/564x/74/82/5b/74825b10d3096266e78abf94aeab0377.jpg",
+    desctiption: "Solitude can help sometimes.",
+    link: "https://i.pinimg.com/564x/74/82/5b/74825b10d3096266e78abf94aeab0377.jpg",
+  },
+  {
+    img: "https://pbs.twimg.com/media/EVxlRaIU0AAAEN_?format=jpg&name=large",
+    desctiption: "His hair is like the sky, white and blue.",
+    link: "https://pbs.twimg.com/media/EVxlRaIU0AAAEN_?format=jpg&name=large",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "anonymous@gmail.com",
+  YOUR_FONE: "(123)4567089)",
+  description:
+    "I might be very late contacting you back, maybe even just forget to but hopefully I can get back to you soon",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  twitter: "https://twitter.com",
+  monster: "https://www.monsterenergy.com/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
